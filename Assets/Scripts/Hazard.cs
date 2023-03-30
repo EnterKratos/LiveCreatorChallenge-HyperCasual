3ae4f4205aae9b54c60684608b08ec14211e82ed
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-03-30 14:36:46 +0100

    Display GameOver screen on hazard collision

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,12 +1,16 @@
+using EnterKratos.ScriptableObjects;
 using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
+    [SerializeField]
+    private GameEvent gameOverEvent;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.rigidbody.CompareTag("Player"))
         {
-            Debug.Log("GameOver");
+            gameOverEvent.Raise();
         }
     }
 }
